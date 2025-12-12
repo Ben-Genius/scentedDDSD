d77ea07ff616f5d983380c3aa8e33027af0e8408
Ben-Genius <bernard.wiafe@afrilogicsolutions.com>
2025-12-12 12:24:04 +0000

    feat: Implement GitHub Pages deployment, add Blog page, and remove howtoorder page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Blog } from './Blog';
 export { Home } from './Home';
 export { Shop } from './Shop';
 export { ProductPage } from './ProductPage';
@@ -7,9 +8,9 @@ export { Checkout } from './Checkout';
 export { AdminInventory } from './AdminInventory';
 
 // Simple placeholders
-export const Collections = () => <div className="pt-24 container mx-auto text-center text-gray-500">Collections Page (Coming Soon)</div>;
+export const Collections = () => <div className=""><Blog/></div>;
 export { About } from './About';
 export const Contact = () => <div className="pt-24 container mx-auto text-center text-gray-500">Contact Us</div>;
-export const HowToOrder = () => <div className="pt-24 container mx-auto text-center text-gray-500">How To Order</div>;
-export const BlogList = () => <div className="pt-24 container mx-auto text-center text-gray-500">Blog</div>;
+export const HowToOrder = () => <div className="pt-24 container mx-auto text-center text-gray-500"></div>;
+export const BlogList = () => <div className="pt-24  mx-auto text-center text-gray-500"> <Blog/> </div>;
 export const BlogPost = () => <div className="pt-24 container mx-auto text-center text-gray-500">Blog Post</div>;
